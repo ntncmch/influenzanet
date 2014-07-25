eaa868e327ab537138a3a451ca684b6581cf4fc4
Anton <anton.camacho@lshtm.ac.uk>
2014-07-25 10:08:07 +0100

    new empirical distributions

--- R/empirical.r
+++ R/empirical.r
@@ -1,27 +1,48 @@
-#'Compute empirical distribution of symptom duration
+#'Compute empirical distributions
 #'
-#'This function computes the empirical distributions of symptom duration from a subset of the weekly survey.
-#'The subset can be set either with \code{subset} or with a vector of warnings to be removed (\code{remove_warnings}). 
-#'Finally, covariates can be summarized for each symptom duration with \code{summarize_covariates}. See example below.
+#'These functions compute several empirical distributions: 
+#'\itemize{
+#'	\item \code{empirical_distribution_symptom_duration} returns the empirical distribution of the time between symptom start and symptom end dates.
+#'	\item \code{empirical_distribution_time_to_report} returns the empirical distribution of the time between the symptom start or end dates (as specified by the \code{what} argument) and the report date.
+#'}
+#'These distribution are computed from a subset of the \code{episode} survey. If this survey is not present, it will be created from the weekly survey using the function \code{\link{summarize_episode}}.
+#'Subsetting is made via the \code{subset} and/or \code{remove_warnings} arguments. See argument description below.
+#'Finally, covariates to be returned are specified with \code{covariates}. See example below.
 #' @param  subset character. Logical expression indicating reports to keep: missing values are taken as false. If present, only episode of illness that verify \code{subset} will be processed. This is mainly to set conditions on the warnings to remove unreliable episodes. See \code{remove_warnings} for an alternative way to subset the weekly survey.
 #' @param  remove_warnings character vector. Names of the warnings to exclude. 
-#' @param  summarize_covariates named character vector. Specify how to summarize covariates for each episode of illness. The names correspond to the summarized covariate variable in the outputted data frame.
+#' @param  covariates character vector of covariates to keep.
 #' @inheritParams summarize_symptoms
-#' @note This function takes \code{symptom_start} and \code{symptom_end} from the first and last report respectively. Thus, only episodes with both information are processed.
+#' @name empirical distribution
+#' @aliases empirical_distribution_symptom_duration
 #' @export
 #' @import dplyr
-#' @return A \code{\link{flunet}} object with the empirical distribution stored as a \code{data.frame} in \code{flunet$log$empirical_distributions$symptom_duration}.
-#' @examples \dontrun{
-#' bad_warnings <- c("W_S_start_too_far", "W_S_start_before_previous_report", "W_S_start_after_S_end", "W_S_start_wrong","W_S_end_too_far", "W_S_end_before_previous_report", "W_S_end_wrong")		
-#' summarize_severity <- c("symptom_severity"="max(na.omit(symptom_severity))")
-#' empirical_distribution_symptom_duration(flunet, remove_warnings=bad_warnings, summarize_covariates=summarize_severity)
+#' @return A \code{\link{flunet}} object with the empirical distributions stored as list of 3 elements in \code{flunet$log$empirical_distributions}. Each list contains
+#'\itemize{
+#'	\item \code{subset} the logical condition used for subsetting the \code{episode} survey. Made from \code{subset} and \code{remove_warnings} arguments.
+#'	\item \code{covariates} copy of the \code{covariates} argument.
+#'	\item \code{distribution} a \code{data.frame} containing the empirical distribution.
 #'}
-empirical_distribution_symptom_duration <- function(flunet, subset=NULL, remove_warnings=NULL, summarize_covariates=NULL) {
+#' @examples \dontrun{
+#' # symptom duration
+#' bad_warnings <- c("W_S_start_too_far", "W_S_start_before_previous_report", "W_S_start_after_S_end", "W_S_start_wrong","W_S_end_too_far", "W_S_end_before_previous_report", "W_S_end_wrong")		
+#' covariates <- "symptom_severity"
+#' flunet <- empirical_distribution_symptom_duration(flunet, remove_warnings=bad_warnings, covariates=covariates)
+#' # time to report symptom start
+#' bad_warnings <- c("W_S_start_too_far", "W_S_start_before_previous_report", "W_S_start_after_S_end", "W_S_start_wrong")
+#' covariates <- c("symptom_severity","first_report_date")
+#' flunet <- empirical_distribution_time_to_report(flunet, what="symptom_start", remove_warnings=bad_warnings, covariates=covariates)
+#' # symptom duration
+#' bad_warnings <- c("W_S_end_too_far", "W_S_end_before_previous_report", "W_S_start_after_S_end", "W_S_end_wrong")
+#' covariates <- c("symptom_severity","last_report_date")
+#' flunet <- empirical_distribution_time_to_report(flunet, what="symptom_end", remove_warnings=bad_warnings, covariates=covariates)
+#'}
+empirical_distribution_symptom_duration <- function(flunet, subset=NULL, remove_warnings=NULL, covariates=NULL) {
 
-	if(is_survey_present(flunet,survey="weekly",warning_message="")){
-		df_weekly <- flunet$surveys$weekly
+	if(is_survey_present(flunet,survey="episode",warning_message=paste(sQuote("summarize_episode"),"is run first"))){
+		df_episode <- flunet$surveys$episode
 	} else {
-		return(flunet)
+		flunet <- summarize_episode(flunet)
+		df_episode <- flunet$surveys$episode
 	}
 
 	# edit subset with remove_warnings
@@ -31,26 +52,69 @@ empirical_distribution_symptom_duration <- function(flunet, subset=NULL, remove_
 	}
 
 	# keep only bout with valid date of symptom onset and end and within subset
-	df_subset <- filter(df_weekly,((!is.na(symptom_start) & position_bout == 1) | (!is.na(symptom_end) & position_bout == length_bout)) & eval(parse(text=subset),df_weekly)) %>%
-	semi_join(df_weekly,.,by=c("person_id","n_bout"))  %>% 
-	group_by(person_id,n_bout) %>% 
-	arrange(comp_time)
-
-	# summarize covariates
-	call_summarize <- parse(text=sprintf("summarize(df_subset,symptom_start=first(symptom_start),symptom_end=last(symptom_end),%s)",paste(paste(names(summarize_covariates),summarize_covariates,sep="="),collapse=",")))	
-	df_summarize <- eval(call_summarize)
+	df_subset <- filter(df_episode,(!is.na(symptom_start) & !is.na(symptom_end) & eval(parse(text=subset),df_episode))) %>% mutate(symptom_duration=symptom_end - symptom_start)
 	
-	# compute symptom duration
-	ans <- df_summarize %>% mutate(symptom_duration=symptom_end - symptom_start) %>% filter(!is.na(symptom_duration))
+	# keep only interesting variables
+	df_subset <- df_subset[c("person_id","n_bout",intersect(names(df_subset),covariates),"symptom_duration")]
 
-	# remove some variables
-	ans <- ans[setdiff(names(ans),c("n_bout","symptom_start","symptom_end"))]
-	
-	flunet$log$empirical_distributions$symptom_duration <- ans
+	flunet$log$empirical_distributions$symptom_duration <- list(subset=subset,covariates=covariates,distribution=df_subset)
 
 	return(flunet)
 }
 
+
+#' @name empirical distribution
+#' @param  what character. Specify what date is reported.
+#' @export
+#' @importFrom plyr rename
+#' @aliases empirical_distribution_time_to_report
+empirical_distribution_time_to_report <- function(flunet, what=c("symptom_start","symptom_end"), subset=NULL, remove_warnings=NULL, covariates=NULL) {
+
+	what <- match.arg(what)
+
+	if(is_survey_present(flunet,survey="episode",warning_message=paste(sQuote("summarize_episode"),"is run first"))){
+		df_episode <- flunet$surveys$episode
+	} else {
+		flunet <- summarize_episode(flunet)
+		df_episode <- flunet$surveys$episode
+	}
+
+	# edit subset with remove_warnings
+	if(!is.null(remove_warnings)){
+		subset_warnings <- paste0("!(",paste(remove_warnings,collapse=" | "),")")
+		# subset_warnings <- paste(remove_warnings,collapse=" | ")
+		subset <- paste(subset,subset_warnings,collapse=" & ")
+	}
+
+	# keep only bout with valid date of symptom onset and end and within subset
+	parse_filter <- sprintf("!is.na(%s)",what)
+	report_date <- switch (what, 
+		symptom_start = "first_report_date",
+		symptom_end = "last_report_date"
+		)
+	parse_mutate <- sprintf("time_to_report_%s = %s - %s",what,report_date,what)
+	
+	new_var <- paste("time_to_report",what,sep="_")
+
+	call <- substitute(
+		df_episode %>% 
+		subset(!is.na(what) & eval(parse(text=subset))) %>% 
+		mutate(toto = report_date - what) %>% 
+		rename(c("toto"=new_var))
+		,list(what=as.name(what),report_date=as.name(report_date),new_var=new_var)
+		)
+	df_subset <- eval(call)
+
+	# keep only interesting variables
+	df_subset <- df_subset[c("person_id","n_bout",intersect(names(df_subset),covariates),new_var)]
+
+	list_dist <- list(list(subset=subset,covariates=covariates,distribution=df_subset))
+
+	flunet$log$empirical_distributions[new_var] <- list_dist
+
+	return(flunet)
+
+}
 
 
 # time to report vs wday for S_start, S_end. dateuration of symptom (by age and type of symptom). baseline heatl score.
@@ -132,7 +196,7 @@ compute_empirical_distribution_for_missing_data <- function(
 	df_S_duration <- join(df_profile,df_S_duration,type="right",by="person_id")
 
 	df_reg <- mutate(df_S_duration,S_duration2=S_duration+0.1,person_id_num=as.numeric(as.factor(person_id)),smoke_bool=smoke_as_logical(smoke))
-	
+
 
 	if(0){
 		df_outlier <- lme_regression(df_reg, formula = "S_duration2~symptom+age_group+smoke_bool+is_risk+vaccine+gender",transformation = "boxcox", max_response = 20, test_dist = FALSE, CI_interval="confidence",suffix= paste0("full_info_SDmax=",S_duration_max),plot_outlier_TS=FALSE,df_data= df_data,df_profile=df_profile,x_var="symptom",fill_var="age_group",use_facet_grid=TRUE,facet_formula="gender~smoke_bool") 
@@ -149,7 +213,7 @@ compute_empirical_distribution_for_missing_data <- function(
 	#df_outlier_wo_6 <- lme_regression(df_reg_wo_age, formula = "S_duration2~symptom+age_group2",transformation = "boxcox", max_response = 20, test_dist = TRUE, CI_interval="prediction",suffix= "full_info_wo",plot_outlier_TS=FALSE,df_data= df_data,df_profile=df_profile,x_var="symptom",fill_var="age_group2") 
 	#tmp <- lme_regression(df_reg_wo_age, formula = "S_duration2~symptom+age_group2",transformation = "boxcox", max_response = 20, test_dist = TRUE, CI_interval="prediction",suffix= "full_info_wo",plot_outlier_TS=FALSE,df_data= df_data,df_profile=df_profile,x_var="symptom",fill_var="age_group2",return_outliers=FALSE) 
 	tmp <- lme_regression(df_reg_age, formula = "S_duration2~symptom+age_group2",transformation = "boxcox", max_response = 20, test_dist = FALSE, CI_interval="prediction",suffix= paste0("full_info_SDmax=",S_duration_max),plot_outlier_TS=FALSE,df_data= df_data,df_profile=df_profile,x_var="symptom",x_lab="symptom severity",fill_var="age_group2",fill_lab="age group",y_lab="symptom duration (in days)",return_outliers=remove_outlier) 
-	
+
 	if(remove_outlier){
 		df_remove <- match_df(df_reg_age, tmp,on=c("person_id","n_bout"))
 		df_reg_age <- diff_df(df_reg_age, df_remove)
